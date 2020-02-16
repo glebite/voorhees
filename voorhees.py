e7f8daa5b39721630e04e27ffefbcb8b6230dea3
Lazlo <glebite@gmail.com>
2020-02-16 16:42:20 -0500

    Search is 'working' - need to cast results properly to make this more useful

--- voorhees.py
+++ voorhees.py
@@ -22,7 +22,7 @@ class Voorhees:
 
     def __str__(self):
         """ string - full string conversion of json_string """
-        return str(json_string)
+        return str(self.incoming_json_string)
     
     def copy(self):
         """
@@ -61,7 +61,7 @@ class Voorhees:
         print(acc_result[:-1])
         return json.loads(acc_result[:-1])
 
-    def search(self, key=None, dump_as_json = False):
+    def search(self, key, dump_as_json = False):
         """
         search - find a key and dump the sub value out or all remaining
 
@@ -81,37 +81,33 @@ class Voorhees:
             [1,2,3,4,5]
             % Voorhees(data).search(dump_as_json = True)
             {'things': [1,2,3,4,5]}
+
+        Return:
+        acc_result - string (up to you to convert it after...)
+                          - empty string if no match (might raise an Exception)
         """
         acc_result = ""
-        def walk(obj, acc_result):
+        def walk(obj, acc_result, key, dump_as_json = False):
             """Recursively search for values of key in JSON tree."""
             if isinstance(obj, dict):
-                acc_result += '{'
                 for k, v in obj.items():
+                    if k == key:
+                        if dump_as_json:
+                            acc_result = '{"' + key + '":' + str(obj[key]) + "}"
+                        else:
+                            acc_result = str(obj[key])
+                        return acc_result
                     if isinstance(v, (dict, list)):
-                        acc_result += '"' + k + '":'                    
-                        acc_result = walk(v, acc_result)
-                    else:
-                        acc_result += '"' + k + '":'                                        
-                        if type(v) == str:
-                            acc_result += '"{}",'.format(v)
-                        if type(v) in [int, bool, float]:
-                            acc_result += '{},'.format(v).lower()
-                acc_result = acc_result[:-1]; acc_result += '},'
+                        acc_result = walk(v, acc_result, key, dump_as_json)
             elif isinstance(obj, list):
-                acc_result += '['
                 for item in obj:
-                    acc_result = walk(item, acc_result)
-                acc_result = acc_result[:-1]; acc_result += '],'
-            else:
-                if type(obj) == str:
-                    acc_result += '"' + str(obj) + '"' + ","
-                if type(obj) in [int, bool, float]:
-                    acc_result += str(obj) + ","
+                    acc_result = walk(item, acc_result, key, dump_as_json)
             return acc_result
-        acc_result = walk(self.incoming_json_string, acc_result)
-        print(acc_result[:-1])
-        return json.loads(acc_result[:-1])
+        acc_result = walk(self.incoming_json_string, acc_result, key, dump_as_json)
+        if acc_result == "":
+            raise KeyError
+        return acc_result
+
     
 if __name__ == "__main__":
     pass
